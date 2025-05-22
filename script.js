bca5542368dd39e06e77d1bca833fd9b8ce30e8a
SOFIA ERN SCHEIDT <sofia_scheidt@estudante.sesisenai.org.br>
2025-05-22 09:21:11 -0300

    terceira etapa

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const fs = require('fs')
-const {v4: uuidv4} = require ('uuid')
+const { v4: uuidv4 } = require('uuid')
+const express = require('express')
+const { error } = require('console')
 
-function registrarLog(nomeAluno){
+const app = express()
+app.use(express.json())
+
+//Registrar logs
+function registrarLog(nomeAluno) {
     const id = uuidv4()
-    const dataHora = new Date(). toISOString()
+    const dataHora = new Date().toISOString()
     const mensagem = `${id}-${dataHora}-${nomeAluno}\n`
-}
+
+    fs.appendFileSync('logs.txt', mensagem)
+    return id
+};
+
+//Adicionar log
+app.post('/logs', (req, res) => {
+    const { nome } = req.body
+    if (!nome) {
+        return res.status(400).json({ error: 'nome do aluno é obrigatório.'})
+        }
+    const id = registrarLog(nome)
+    res.json({id, mensagem:'Log registrado com sucesso!'})
+    });
+
+
